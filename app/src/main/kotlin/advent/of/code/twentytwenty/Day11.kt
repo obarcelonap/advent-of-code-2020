b01ef346af96b509540e2dc3878074e5b8bd7e50
Oriol Barcelona <obarcelonap@gmail.com>
2020-12-11 19:20:57 +0100

    Resolved day11 second puzzle

--- app/src/main/kotlin/advent/of/code/twentytwenty/Day11.kt
+++ app/src/main/kotlin/advent/of/code/twentytwenty/Day11.kt
@@ -2,9 +2,14 @@ package advent.of.code.twentytwenty
 
 fun main(args: Array<String>) {
     val seatPlan = newSeatPlan(getResourceAsText("/day11-input"))
-    val stabilizedSeatPlan = stabilizeSeatPlan(seatPlan)
-    val occupiedSeats = countOccupiedSeats(stabilizedSeatPlan)
-    println("Part 1: found $occupiedSeats occupied seats in seat plan")
+    val stabilizedSeatPlanPart1 = stabilizeSeatPlan(seatPlan, ::countAdjacentOccupiedSeats, 4)
+    val occupiedSeatsPart1 = countOccupiedSeats(stabilizedSeatPlanPart1)
+    println("Part 1: found $occupiedSeatsPart1 occupied seats in seat plan")
+
+
+    val stabilizedSeatPlanPart2 = stabilizeSeatPlan(seatPlan, ::countFirstOccupiedSeatInAllDirections, 5)
+    val occupiedSeatsPart2 = countOccupiedSeats(stabilizedSeatPlanPart2)
+    println("Part 2: found $occupiedSeatsPart2 occupied seats in seat plan")
 }
 
 abstract class Position(val icon: Char) {
@@ -19,13 +24,14 @@ abstract class Seat(icon: Char) : Position(icon)
 object Occupied : Seat('#')
 object Empty : Seat('L')
 typealias SeatPlan = List<List<Position>>
+typealias CountSeats = (x: Int, y: Int) -> Int
 
-fun stabilizeSeatPlan(seatPlan: SeatPlan) = generateSequence(seatPlan, {
-    val newSeatPlan = iterateSeatPlan(it)
-    if (newSeatPlan == it) null else newSeatPlan
-})
-        .last()
-
+fun stabilizeSeatPlan(seatPlan: SeatPlan, countSeats: (seatPlan: SeatPlan) -> CountSeats, occupiedLimit: Int) =
+        generateSequence(seatPlan, {
+            val newSeatPlan = iterate(it, countSeats(it), occupiedLimit)
+            if (newSeatPlan == it) null else newSeatPlan
+        })
+                .last()
 
 fun newSeatPlan(text: String): SeatPlan = text.lines()
         .map {
@@ -38,32 +44,17 @@ fun newSeatPlan(text: String): SeatPlan = text.lines()
             }
         }
 
-
 fun countOccupiedSeats(seatPlan: SeatPlan) =
         seatPlan.flatten()
                 .sumBy { it.countOccupied() }
 
-fun iterateSeatPlan(seatPlan: SeatPlan): SeatPlan {
-    val maxX = seatPlan.size
-    val maxY = seatPlan[0].size
-
-    fun countOcuppied(x: Int, y: Int): Int =
-            if (x !in 0 until maxX || y !in 0 until maxY) 0
-            else seatPlan[x][y].countOccupied()
-
-    fun countAdjacentOccupiedSeats(x: Int, y: Int): Int =
-            listOf(
-                    Pair(x - 1, y - 1), Pair(x, y - 1), Pair(x + 1, y - 1),
-                    Pair(x - 1, y), Pair(x + 1, y),
-                    Pair(x - 1, y + 1), Pair(x, y + 1), Pair(x + 1, y + 1)
-            )
-                    .map { (x, y) -> countOcuppied(x, y) }
-                    .sum()
+private fun iterate(seatPlan: SeatPlan, countSeats: CountSeats, occupiedLimit: Int): SeatPlan {
+    val (maxX, maxY) = size(seatPlan)
 
     fun iteratePosition(x: Int, y: Int): Position =
             when (val currentPosition = seatPlan[x][y]) {
-                is Empty -> if (countAdjacentOccupiedSeats(x, y) == 0) Occupied else Empty
-                is Occupied -> if (countAdjacentOccupiedSeats(x, y) >= 4) Empty else Occupied
+                is Empty -> if (countSeats(x, y) == 0) Occupied else Empty
+                is Occupied -> if (countSeats(x, y) >= occupiedLimit) Empty else Occupied
                 else -> currentPosition
             }
 
@@ -77,3 +68,42 @@ fun iterateSeatPlan(seatPlan: SeatPlan): SeatPlan {
             }
             .toList()
 }
+
+fun countAdjacentOccupiedSeats(seatPlan: SeatPlan): CountSeats {
+    return fun(x: Int, y: Int): Int = adjacentSeats(x, y, seatPlan)
+            .map { (x, y) -> seatPlan[x][y] }
+            .count { it is Occupied }
+}
+
+private fun adjacentSeats(x: Int, y: Int, seatPlan: SeatPlan): List<Pair<Int, Int>> {
+    val seatPlanRange = inRange(seatPlan)
+
+    return listOf(
+            Pair(x - 1, y - 1), Pair(x, y - 1), Pair(x + 1, y - 1),
+            Pair(x - 1, y), Pair(x + 1, y),
+            Pair(x - 1, y + 1), Pair(x, y + 1), Pair(x + 1, y + 1)
+    )
+            .filter { (x, y) -> seatPlanRange(x, y) }
+}
+
+private fun inRange(seatPlan: SeatPlan): (x: Int, y: Int) -> Boolean {
+    val (maxX, maxY) = size(seatPlan)
+    return fun(x, y) = x in 0 until maxX && y in 0 until maxY
+}
+
+fun countFirstOccupiedSeatInAllDirections(seatPlan: SeatPlan): CountSeats =
+        fun(x: Int, y: Int): Int = adjacentSeats(x, y, seatPlan)
+                .map { (adjacentX, adjacentY) -> navigateToSeat(Pair(adjacentX, adjacentY), Pair(adjacentX - x, adjacentY - y), seatPlan) }
+                .count { it is Occupied }
+
+fun navigateToSeat(position: Pair<Int, Int>, direction: Pair<Int, Int>, seatPlan: SeatPlan): Seat? {
+    if (!inRange(seatPlan)(position.first, position.second)) {
+        return null
+    }
+    return when (val current = seatPlan[position.first][position.second]) {
+        is Seat -> current
+        else -> navigateToSeat(Pair(position.first + direction.first, position.second + direction.second), direction, seatPlan)
+    }
+}
+
+private fun size(seatPlan: SeatPlan): Pair<Int, Int> = Pair(seatPlan.size, seatPlan[0].size)
--- app/src/test/kotlin/advent/of/code/twentytwenty/Day11Test.kt
+++ app/src/test/kotlin/advent/of/code/twentytwenty/Day11Test.kt
@@ -19,7 +19,7 @@ class Day11Test {
             L.LLLLL.LL
         """.trimIndent())
 
-        val stabilizedSeatPlan = stabilizeSeatPlan(seatPlan)
+        val stabilizedSeatPlan = stabilizeSeatPlan(seatPlan, ::countAdjacentOccupiedSeats, 4)
 
         assertEquals(
                 newSeatPlan("""
@@ -39,6 +39,89 @@ class Day11Test {
 
         val occupiedSeats = countOccupiedSeats(stabilizedSeatPlan)
         assertEquals(37, occupiedSeats)
+    }
+
+    @Test
+    fun shouldCountEight_whenFirstSeatInAllDirectionsIsOccupied() {
+        val seatPlan = newSeatPlan("""
+            .......#.
+            ...#.....
+            .#.......
+            .........
+            ..#L....#
+            ....#....
+            .........
+            #........
+            ...#.....
+        """.trimIndent())
+
+        val count = countFirstOccupiedSeatInAllDirections(seatPlan)(4, 3)
+        assertEquals(8, count)
+    }
+
+    @Test
+    fun shouldCountZero_whenEmptySeatsAreNotTheFirstOnes() {
+        val seatPlan = newSeatPlan("""
+            .............
+            .L.L.#.#.#.#.
+            .............
+        """.trimIndent())
+
+        val count = countFirstOccupiedSeatInAllDirections(seatPlan)(1, 1)
+        assertEquals(0, count)
+    }
+
+    @Test
+    fun shouldCountZero_whenNoSeatsAreInEightDirections() {
+        val seatPlan = newSeatPlan("""
+            .##.##.
+            #.#.#.#
+            ##...##
+            ...L...
+            ##...##
+            #.#.#.#
+            .##.##.
+        """.trimIndent())
+
+        val count = countFirstOccupiedSeatInAllDirections(seatPlan)(3, 3)
+        assertEquals(0, count)
+    }
+
+    @Test
+    fun shouldSatisfyPuzzleExampleOfPart2() {
+        val seatPlan = newSeatPlan("""
+            L.LL.LL.LL
+            LLLLLLL.LL
+            L.L.L..L..
+            LLLL.LL.LL
+            L.LL.LL.LL
+            L.LLLLL.LL
+            ..L.L.....
+            LLLLLLLLLL
+            L.LLLLLL.L
+            L.LLLLL.LL
+        """.trimIndent())
+
+        val stabilizedSeatPlan = stabilizeSeatPlan(seatPlan, ::countFirstOccupiedSeatInAllDirections, 5)
+
+        assertEquals(
+                newSeatPlan("""
+                    #.L#.L#.L#
+                    #LLLLLL.LL
+                    L.L.L..#..
+                    ##L#.#L.L#
+                    L.L#.LL.L#
+                    #.LLLL#.LL
+                    ..#.L.....
+                    LLL###LLL#
+                    #.LLLLL#.L
+                    #.L#LL#.L#
+                """.trimIndent()),
+                stabilizedSeatPlan
+        )
+
+        val occupiedSeats = countOccupiedSeats(stabilizedSeatPlan)
+        assertEquals(26, occupiedSeats)
     }
 
 }
