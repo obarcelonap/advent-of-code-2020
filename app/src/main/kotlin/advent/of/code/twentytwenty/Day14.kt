e469a84db237f52193d04dd6e7c6d14cf842da1d
Oriol Barcelona <obarcelonap@gmail.com>
2020-12-14 13:58:58 +0100

    Resolved day14 second puzzle

--- app/src/main/kotlin/advent/of/code/twentytwenty/Day14.kt
+++ app/src/main/kotlin/advent/of/code/twentytwenty/Day14.kt
@@ -1,17 +1,23 @@
 package advent.of.code.twentytwenty
 
 import java.lang.Long.parseLong
+import java.lang.Long.toBinaryString
 
 typealias Mask = String
 typealias Memory = Map<Long, Long>
 typealias MaskedMemory = Pair<Memory, Mask>
 
 fun main() {
-    val sum = memorySum(getResourceAsText("/day14-input"))
-    println("Part1: final memory sum is $sum")
+    val input = getResourceAsText("/day14-input")
+    val part1Sum = memorySum(input, ::valueBitMaskDecoder)
+    println("Part1: final memory sum is $part1Sum")
+
+    val part2Sum = memorySum(input, ::memoryAddressDecoder)
+    println("Part2: final memory sum is $part2Sum")
+
 }
 
-fun memorySum(input: String): Long {
+fun memorySum(input: String, decoder: (mem: MaskedMemory, address: Long, value: Long) -> MaskedMemory): Long {
     val initialMask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     val initialMemory = emptyMap<Long, Long>()
 
@@ -20,9 +26,9 @@ fun memorySum(input: String): Long {
                 if (line.startsWith("mask")) {
                     return { (mem) -> Pair(mem, parseMask(line)) }
                 }
-                return { (mem, mask) ->
+                return { mem ->
                     val (address, value) = parseMemoryWrite(line)
-                    Pair(mem + Pair(address, applyMask(value, mask)), mask)
+                    decoder(mem, address, value)
                 }
             })
             .fold(Pair(initialMemory, initialMask), { mem, instruction -> instruction(mem) })
@@ -31,27 +37,50 @@ fun memorySum(input: String): Long {
             .sum()
 }
 
-
-fun applyMask(value: Long, mask: String): Long {
-    val onesMask = mask.map { character ->
-        when (character) {
-            '1' -> '1'
-            else -> '0'
-        }
-    }
-            .joinToBinaryLong()
-
-    val zerosMask = mask.map { character ->
-        when (character) {
-            '0' -> '1'
-            else -> '0'
-        }
-    }
-            .joinToBinaryLong()
-            .inv()
-
-    return value or onesMask and zerosMask
+fun valueBitMaskDecoder(maskedMemory: MaskedMemory, address: Long, value: Long): MaskedMemory {
+    val (mem, mask) = maskedMemory
+    return Pair(mem + Pair(address, applyMask(value, mask)), mask)
 }
+
+fun memoryAddressDecoder(maskedMemory: MaskedMemory, address: Long, value: Long): MaskedMemory {
+    val (mem, mask) = maskedMemory
+
+    val maskedAddr = toBinaryString(address or onesMask(mask))
+            .padStart(mask.length, '0')
+            .mapIndexed { index, character -> if (mask[index] == 'X') 'X' else character }
+            .joinToString("")
+
+    fun generateAddresses(value: String): List<String> {
+        if (!value.contains('X')) {
+            return listOf(value)
+        }
+        return generateAddresses(value.replaceFirst('X', '0')) + generateAddresses(value.replaceFirst('X', '1'))
+    }
+
+    val newMemory = mem + generateAddresses(maskedAddr)
+            .map { Pair(parseLong(it, 2), value) }
+
+    return Pair(newMemory, mask)
+}
+
+fun applyMask(value: Long, mask: String): Long = value or onesMask(mask) and zerosMask(mask)
+
+private fun zerosMask(mask: String) = mask.map { character ->
+    when (character) {
+        '0' -> '1'
+        else -> '0'
+    }
+}
+        .joinToBinaryLong()
+        .inv()
+
+private fun onesMask(mask: String): Long = mask.map { character ->
+    when (character) {
+        '1' -> '1'
+        else -> '0'
+    }
+}
+        .joinToBinaryLong()
 
 
 private fun <T> Iterable<T>.joinToBinaryLong(): Long {
--- app/src/test/kotlin/advent/of/code/twentytwenty/Day14Test.kt
+++ app/src/test/kotlin/advent/of/code/twentytwenty/Day14Test.kt
@@ -36,7 +36,7 @@ class Day14Test {
     }
 
     @Test
-    fun shouldValidaterFirstExample() {
+    fun shouldValidateFirstExample() {
         val input = """
             mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
             mem[8] = 11
@@ -44,8 +44,22 @@ class Day14Test {
             mem[8] = 0
         """.trimIndent()
 
-        val sum = memorySum(input)
+        val sum = memorySum(input, ::valueBitMaskDecoder)
 
         assertEquals(165, sum)
+    }
+
+    @Test
+    fun shouldValidateSecondExample() {
+        val input = """
+            mask = 000000000000000000000000000000X1001X
+            mem[42] = 100
+            mask = 00000000000000000000000000000000X0XX
+            mem[26] = 1
+        """.trimIndent()
+
+        val sum = memorySum(input, ::memoryAddressDecoder)
+
+        assertEquals(208, sum)
     }
 }
