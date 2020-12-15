1f3a560b827b3f5b406a6a331f8d934ca774b23f
Oriol Barcelona <obarcelonap@gmail.com>
2020-12-15 11:43:06 +0100

    Resolved day15 second part

--- app/src/main/kotlin/advent/of/code/twentytwenty/Day15.kt
+++ app/src/main/kotlin/advent/of/code/twentytwenty/Day15.kt
@@ -1,9 +1,12 @@
 package advent.of.code.twentytwenty
 
 fun main() {
-    val numberSpoken = numberSpokenAt(2020, listOf(1, 12, 0, 20, 8, 16))
-    println("Part1: number spoken at 2020 is $numberSpoken")
+    val startingNumbers = listOf(1, 12, 0, 20, 8, 16)
+    val numberSpokenAt2020 = numberSpokenAt(2020, startingNumbers)
+    println("Part1: number spoken at 2020 is $numberSpokenAt2020")
 
+    val numberSpokenAt30000000 = numberSpokenAt(30000000, startingNumbers)
+    println("Part2: number spoken at 30000000 is $numberSpokenAt30000000")
 }
 
 fun numberSpokenAt(targetTurn: Int, startingNumbers: List<Int>): Int {
@@ -11,7 +14,7 @@ fun numberSpokenAt(targetTurn: Int, startingNumbers: List<Int>): Int {
         return startingNumbers[targetTurn - 1]
     }
 
-    fun numberSpoken(numbers: Map<Int, Int>, lastNumber: Int, turn: Int = 1): Int {
+    tailrec fun numberSpoken(numbers: MutableMap<Int, Int>, lastNumber: Int, turn: Int = 1): Int {
         if (targetTurn == turn) {
             return lastNumber
         }
@@ -20,11 +23,15 @@ fun numberSpokenAt(targetTurn: Int, startingNumbers: List<Int>): Int {
                 if (numbers.containsKey(lastNumber)) turn - numbers.getValue(lastNumber)
                 else 0
 
-        return numberSpoken(numbers + Pair(lastNumber, turn), nextNumber, turn + 1)
+        numbers[lastNumber] = turn
+        return numberSpoken(numbers, nextNumber, turn + 1)
     }
 
     val numbers = startingNumbers.dropLast(1)
-            .mapIndexed { turn, number -> Pair(number, turn + 1) }.toMap()
+            .mapIndexed { turn, number -> Pair(number, turn + 1) }
+            .toMap()
+            .toMutableMap()
+
     val nextNumber = startingNumbers.last()
     return numberSpoken(numbers, nextNumber, numbers.size + 1)
 }
--- app/src/test/kotlin/advent/of/code/twentytwenty/Day15Test.kt
+++ app/src/test/kotlin/advent/of/code/twentytwenty/Day15Test.kt
@@ -21,4 +21,21 @@ class Day15Test {
                     assertEquals(expectedNumber, actualNumber)
                 }
     }
+
+    @Test
+    fun shouldSatisfyExamplesOfPart2() {
+        listOf(
+                Pair(listOf(0, 3, 6), 175594),
+                Pair(listOf(1, 3, 2), 2578),
+                Pair(listOf(2, 1, 3), 3544142),
+                Pair(listOf(1, 2, 3), 261214),
+                Pair(listOf(2, 3, 1), 6895259),
+                Pair(listOf(3, 2, 1), 18),
+                Pair(listOf(3, 1, 2), 362)
+        )
+                .forEach { (startingNumbers, expectedNumber) ->
+                    val actualNumber = numberSpokenAt(30000000, startingNumbers)
+                    assertEquals(expectedNumber, actualNumber)
+                }
+    }
 }
